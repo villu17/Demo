aecea6ff2b52d00794fae225d59c2e079f2466b4
Vishal Jariwala <vishaljariwala31@gmail.com>
2023-10-29 10:08:27 +0530

    Design and Bit Logic Change

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react';
-import { Card, CardBody, CardTitle, Table } from "reactstrap";
 import Button from '../../utils/Button';
 import Loading from '../../utils/Loading';
+
+interface UserData {
+    name: string;
+    email: string;
+    image: string;
+};
 
 
 const Dashboard = () => {
 
     //State
     const [isLoading, setIsLoading] = useState(false);
-    const [userData, setUserData] = useState<any[]>([])
+    const [userData, setUserData] = useState<UserData>({
+        name: "",
+        email: "",
+        image: ""
+    })
     const [callAPI, setCallAPI] = useState(false);
 
 
@@ -17,15 +26,16 @@ const Dashboard = () => {
         const getUserData = async () => {
             setIsLoading(true);
             await axios.get(`${process.env.REACT_APP_API_ENDPOINT}`).then((response: any) => {
-                let stroedData = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData") || "") : [];
-                stroedData = [...stroedData, response ?.data ?.results ?.[0]];
+                let stroedData = [];
+                stroedData = response ?.data ?.results ?.[0];
                 localStorage.setItem('userData', JSON.stringify(stroedData));
 
-                let finalData: any = [];
-                stroedData ?.forEach((item: any) => {
-                    finalData ?.push({ name: item.name, email: item.email, thumbnail: item.picture ?.thumbnail })
-                })
-                setUserData(finalData);
+                setUserData({
+                    ...userData,
+                    name: `${stroedData ?.name ?.title} ${stroedData ?.name ?.first} ${stroedData ?.name ?.last}`,
+                    email: stroedData ?.email,
+                    image: stroedData ?.picture ?.large,
+                });
             })
                 .catch((error: any) => {
                     console.log("Error Comming !!!!!")
@@ -41,50 +51,22 @@ const Dashboard = () => {
     return (
         <Fragment>
             {isLoading && <Loading />}
-            <div className="p-5">
-                <Card className="border-0">
-                    <CardBody>
-                        <div className="d-flex justify-content-between">
-                            <CardTitle tag="h5">Users Details</CardTitle>
-                            <Button variant="resetSqare" onClick={() => setCallAPI(!callAPI)} disabled={isLoading} title={"Refersh User List"} />
-                        </div>
-                        <div className="custHeight scrollWidth mt-3">
-                            <Table className="no-wrap mt-3 align-middle" responsive borderless>
-                                <thead>
-                                    <tr>
-                                        <th >Name</th>
-                                        <th className="td-cntr">Email</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        userData && userData ?.map((item: any, index: number) => (
-                                            <tr key={index} className="border-top">
-                                                <td >
-                                                    <div className="d-flex align-items-center p-2">
-                                                        <img
-                                                            src={item ?.thumbnail}
-                                                            className="rounded-circle"
-                                                            alt="avatar"
-                                                            width="45"
-                                                            height="45"
-                                                        />
-                                                        <div className="ms-3">
-                                                            <h6 className="mb-0">{item ?.name ?.title} {item ?.name ?.first} {item ?.name ?.last}</h6>
+            <div className="p-4">
 
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="td-cntr"><h6>{item ?.email}</h6></td>
-                                            </tr>
-                                        ))
-            }
-
-                                </tbody>
-                            </Table>
+                <div className="d-flex justify-content-between align-items-center border-bottom title">
+                    <h5 className="mb-0">Users Details</h5>
+                    <Button variant="resetSqare" onClick={() => setCallAPI(!callAPI)} disabled={isLoading} title={"Refersh User List"} />
+                </div>
+                <div className="justify-content-center d-flex mt-5">
+                    <div className="profile">
+                        <div className="profile-image">
+                            <img src={userData ?.image} alt="avatar" />
                         </div>
-                    </CardBody>
-                </Card>
+                        <h2 className="profile-username">{userData ?.name}</h2>
+                        <small className="profile-user-handle">{userData ?.email}</small>
+                    </div>
+                </div>
+
             </div>
         </Fragment>
     )
